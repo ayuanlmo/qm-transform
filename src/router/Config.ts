dc6f1fe052535092cd1ea9e69b3305d936a6315a
ayuanlmo <ayuanlmo@outlook.com>
2026-02-09 03:25:08 +0800

    feat：暂时隐藏未实现的模块

--- src/router/Config.ts
+++ src/router/Config.ts
@@ -1,8 +1,8 @@
 import type {FC} from "react";
 import VideoTransform from "../views/VideoTransform";
 import AudioTransform from "../views/AudioTransform";
-import VideoConcat from "../views/VideoConcat";
-import VideoCompress from "../views/VideoCompress";
+// import VideoConcat from "../views/VideoConcat";
+// import VideoCompress from "../views/VideoCompress";
 
 export interface IRouterItem {
     name: string;
@@ -23,17 +23,18 @@ export default [
         path: '/audio-transform',
         icon: '/react-app-static/ico/audio-transform.svg',
         template: AudioTransform
-    },
-    {
-        name: 'menu.videoConcat',
-        path: '/video-concat',
-        icon: '/react-app-static/ico/vide-concat.svg',
-        template: VideoConcat
-    },
-    {
-        name: 'menu.videoCompress',
-        path: '/video-compress',
-        icon: '/react-app-static/ico/video-compress.svg',
-        template: VideoCompress
     }
+    // ,
+    // {
+    //     name: 'menu.videoConcat',
+    //     path: '/video-concat',
+    //     icon: '/react-app-static/ico/vide-concat.svg',
+    //     template: VideoConcat
+    // },
+    // {
+    //     name: 'menu.videoCompress',
+    //     path: '/video-compress',
+    //     icon: '/react-app-static/ico/video-compress.svg',
+    //     template: VideoCompress
+    // }
 ];
